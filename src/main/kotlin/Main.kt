ca160b772108b81785af6bf54e0124ba9d347761
Ken Gullaksen <ken@glxn.net>
2022-12-03 16:22:09 +0100

    day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -105,7 +105,37 @@ sealed interface AOC {
         }
     }
 
+    object Day3 : AOC {
+        val priorities = (('a'..'z') + ('A'..'Z'))
+            .mapIndexed { index, char -> char to index + 1 }
+            .toMap()
+
+        override fun solve(): String {
+            val rucksacks = "day3.input".read().split("\n")
+
+            val duplicates = rucksacks.map {
+                val (first, second) = it.chunked(it.length / 2)
+                first.toSet() intersect second.toSet()
+            }
+
+            val groups = rucksacks.chunked(3).map { group ->
+                group[0].toSet() intersect group[1].toSet() intersect group[2].toSet()
+            }
+
+            return """
+                |## Day 3
+                |* Part 1: 
+                | * ${duplicates.flatten().sumOf { priorities[it] as Int }}
+                |* Part 2: 
+                | * ${groups.flatten().sumOf { priorities[it] as Int }}
+            """.trimMargin()
+        }
+    }
+
 
 }
 
 fun String.read() = AOC::class.java.getResource(this)!!.readText()
+//fun main(vararg arg: String) {
+//    println(AOC.Day3.solve())
+//}
