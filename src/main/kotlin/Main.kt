0bfa4fdb2e6d2cd0199f5e3fe2f25b52cbe82a2f
Ken Gullaksen <ken@glxn.net>
2022-12-04 12:06:58 +0100

    day 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -132,10 +132,37 @@ sealed interface AOC {
         }
     }
 
+    object Day4 : AOC {
+        private fun String.range(): IntRange = split('-').first().toInt()..split('-').last().toInt()
+        operator fun String.component1() = split(",").first()
+        operator fun String.component2() = split(",").last()
+
+        override fun solve(): String {
+            val elves = "day4.input".read().split("\n")
+                .map { (first, last) -> first.range() to last.range() }
+
+            val completeOverlaps = elves.count { (first, last) ->
+                (first - last).isEmpty() || (last - first).isEmpty()
+            }
+
+            val anyOverlap = elves.count { (first, last) ->
+                (first intersect last).isNotEmpty()
+            }
+
+            return """
+                |## Day 4
+                |* Part 1: 
+                | * $completeOverlaps
+                |* Part 2: 
+                | * $anyOverlap
+            """.trimMargin()
+        }
+    }
+
 
 }
 
 fun String.read() = AOC::class.java.getResource(this)!!.readText()
 //fun main(vararg arg: String) {
-//    println(AOC.Day3.solve())
+//    println(AOC.Day4.solve())
 //}
